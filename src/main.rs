c09896e1a38a202254ad7ad9029876c95fbe1183
Nils Nahooy <nils.nahooy@live.com>
2024-06-04 22:12:09 +0200

    draw player and bg with renderer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
 use sdl2::video::Window;
 use std::time::Duration;
@@ -53,6 +54,46 @@ fn main() -> Result<(), String> {
             let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
             Ok(Renderer { canvas })
         }
+
+        pub fn draw(&mut self, context: &GameContext) -> Result<(), String> {
+            self.draw_bg(context);
+            self.draw_player(context)?;
+            self.canvas.present();
+
+            Ok(())
+        }
+
+        fn draw_bg(&mut self, context: &GameContext) {
+            let color = match context.state {
+                GameState::Playing => Color::RGB(0, 0, 0),
+                GameState::Paused => Color::RGB(30, 30, 30),
+                GameState::Over => Color::RGB(60, 60, 60),
+            };
+
+            self.canvas.set_draw_color(color);
+            self.canvas.clear();
+        }
+
+        fn draw_player(&mut self, context: &GameContext) -> Result<(), String> {
+            self.canvas.set_draw_color(Color::GREEN);
+
+            for point in &context.player_position {
+                self.draw_point(point)?;
+            }
+            Ok(())
+        }
+
+        fn draw_point(&mut self, point: &Point) -> Result<(), String> {
+            let Point(x, y) = point;
+            self.canvas.fill_rect(Rect::new(
+                x * DOT_SIZE_IN_PXS as i32,
+                y * DOT_SIZE_IN_PXS as i32,
+                DOT_SIZE_IN_PXS,
+                DOT_SIZE_IN_PXS,
+            ))?;
+
+            Ok(())
+        }
     }
 
     let sdl_context = sdl2::init()?;
@@ -73,7 +114,7 @@ fn main() -> Result<(), String> {
 
     let mut context = GameContext::new();
 
-    let mut renderer = Renderer::new(window);
+    let mut renderer = Renderer::new(window)?;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -86,6 +127,8 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
+
+        renderer.draw(&context)?;
 
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
         // The rest of the game loop goes here...
