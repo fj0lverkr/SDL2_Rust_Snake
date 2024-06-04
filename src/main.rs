ef40e76a72b7015ecfb2ca60d658b38cf91035a9
Nils Nahooy <nils.nahooy@live.com>
2024-06-04 17:34:27 +0200

    initial setup for snake game

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,52 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use std::time::Duration;
 
+const GRID_X_SIZE: u32 = 40;
+const GRID_Y_SIZE: u32 = 30;
+const DOT_SIZE_IN_PXS: u32 = 20;
+
 fn main() -> Result<(), String> {
+    pub enum GameState {
+        Playing,
+        Paused,
+        Over,
+    }
+    pub enum PlayerDirection {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    pub struct Point(pub i32, pub i32);
+
+    pub struct GameContext {
+        pub player_position: Vec<Point>,
+        pub player_direction: PlayerDirection,
+        pub food: Point,
+        pub state: GameState,
+    }
+
+    impl GameContext {
+        pub fn new() -> GameContext {
+            GameContext {
+                player_position: vec![Point(3, 1), Point(2, 1), Point(1, 1)],
+                player_direction: PlayerDirection::Right,
+                state: GameState::Paused,
+                food: Point(3, 3),
+            }
+        }
+    }
+
     let sdl_context = sdl2::init()?;
     let video_subsys = sdl_context.video()?;
 
     let window = video_subsys
-        .window("SDL2 Rust demo", 800, 600)
+        .window(
+            "Snake Game SDL2 Rust",
+            GRID_X_SIZE * DOT_SIZE_IN_PXS,
+            GRID_Y_SIZE * DOT_SIZE_IN_PXS,
+        )
         .position_centered()
         .opengl()
         .build()
