cbfd4d3035474e832fbcb7e34d991870ddb245c8
Nils Nahooy <nils.nahooy@live.com>
2024-06-05 17:24:24 +0200

    basic movement for snake

--- src/game_context.rs
+++ src/game_context.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 pub enum GameState {
     Playing,
     Paused,
@@ -10,13 +12,27 @@ pub enum PlayerDirection {
     Right,
 }
 
+#[derive(Copy, Clone)]
 pub struct Point(pub i32, pub i32);
+
+impl Add<Point> for Point {
+    type Output = Point;
+    fn add(self, rhs: Point) -> Self::Output {
+        Point(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
 
 pub struct GameContext {
     pub player_position: Vec<Point>,
     pub player_direction: PlayerDirection,
     pub food: Point,
     pub state: GameState,
+}
+
+impl Default for GameContext {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl GameContext {
@@ -29,5 +45,18 @@ impl GameContext {
         }
     }
 
-    pub fn do_next_tick(&mut self) {}
+    pub fn do_next_tick(&mut self) {
+        let current_player_head_pos = self.player_position.first().unwrap();
+        let next_player_head_pos = match self.player_direction {
+            PlayerDirection::Up => *current_player_head_pos + Point(0, -1),
+            PlayerDirection::Down => *current_player_head_pos + Point(0, 1),
+            PlayerDirection::Left => *current_player_head_pos + Point(-1, 0),
+            PlayerDirection::Right => *current_player_head_pos + Point(1, 0),
+        };
+
+        self.player_position.pop();
+        self.player_position.reverse();
+        self.player_position.push(next_player_head_pos);
+        self.player_position.reverse();
+    }
 }
